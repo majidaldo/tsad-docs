a1d8cea1a46df7b443850f49365552974e81e829
Majid d <majidaldosari-github@yahoo.com>
2015-11-12 14:09:25 -0500

    correct tight with sizes

--- figs.py
+++ figs.py
@@ -1,8 +1,9 @@
 import numpy as np
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
 
-
+from math import sqrt
 
 class fig(object):
     def data(self):pass
@@ -10,6 +11,10 @@ class fig(object):
     def plot(self):pass
     def save(self):pass
 
+def forceAspect(ax,aspect=1):
+    im = ax.get_images();
+    extent =  im[0].get_extent()
+    ax.set_aspect(abs((extent[1]-extent[0])/(extent[3]-extent[2]))/aspect)
 
 class anomtype(fig):
     T=200
@@ -17,18 +22,16 @@ class anomtype(fig):
         po.axes.get_xaxis().set_ticklabels([])
         po.axes.get_yaxis().set_ticklabels([])
         po.axes.get_xaxis().set_label_text('$t$')
+        plt.tight_layout(pad=0)
+        latexify(6,ratio=.3) #w,r=h*w
         return po
     def plot(self):
         return self.style(plt.plot(self.data())[0])
     def save(self):
         self.plot().figure.savefig(
             self.__class__.__name__+'.pdf'
-            ,bbox_inches='tight'
-            ,pad_inches=0
         )
 
-#todo set golden ratio
-#figure.savefig('asdfsf.pdf')
 
 class trivial(anomtype):
     def data(self):
@@ -36,3 +39,75 @@ class trivial(anomtype):
         ys[int(self.T*.5)]=1.5
         return ys
 
+
+
+    
+def latexify(fig_width=None
+             , fig_height=None
+             ,ratio='golden'
+             , columns=1):
+    """Set up matplotlib's RC params for LaTeX plotting.
+    Call this before plotting a figure.
+
+    Parameters
+    ----------
+    fig_width : float, optional, inches
+    fig_height : float,  optional, inches
+    columns : {1, 2}
+    """
+
+    # code adapted from http://www.scipy.org/Cookbook/Matplotlib/LaTeX_Examples
+
+    # Width and max height in inches for IEEE journals taken from
+    # computer.org/cms/Computer.org/Journal%20templates/transactions_art_guide.pdf
+
+    assert(columns in [1,2])
+
+    if fig_width is None:
+        fig_width = 3.39 if columns==1 else 6.9 # width in inches
+
+    if fig_height is None:
+        if ratio=='golden':
+            ar = (sqrt(5)-1.0)/2.0    # Aesthetic ratio
+        else:
+            ar=ratio
+            fig_height = fig_width*ar # height in inches
+
+    MAX_HEIGHT_INCHES = 8.0
+    if fig_height > MAX_HEIGHT_INCHES:
+        print("WARNING: fig_height too large:" + str(fig_height) + 
+              "so will reduce to" + str(MAX_HEIGHT_INCHES) + "inches.")
+        fig_height = MAX_HEIGHT_INCHES
+
+    params = {'backend': 'ps',
+              'text.latex.preamble': ['\usepackage{gensymb}'],
+              'axes.labelsize': 10, # fontsize for x and y labels (was 10)
+              'axes.titlesize': 10,
+              'text.fontsize': 10, # was 10
+              'legend.fontsize': 10, # was 10
+              'xtick.labelsize': 10,
+              'ytick.labelsize': 10,
+              'text.usetex': True,
+              'figure.figsize': [fig_width,fig_height],
+              'font.family': 'serif'
+    }
+
+    matplotlib.rcParams.update(params)
+
+
+def format_axes(ax):
+
+    for spine in ['top', 'right']:
+        ax.spines[spine].set_visible(False)
+
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_color(SPINE_COLOR)
+        ax.spines[spine].set_linewidth(0.5)
+
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_tick_params(direction='out', color=SPINE_COLOR)
+
+    return ax
