88e3d92eba82dc6a9bb997d566cdd0f0f6722503
Majid d <majidaldosari-github@yahoo.com>
2016-01-18 14:44:49 -0500

    tbls init

--- tbls/tbls.py
+++ tbls/tbls.py
@@ -4,4 +4,39 @@ import tsad
 import analysis
 import data
 
+import os
 
+#todo def register
+
+
+class tbl(object):
+    kwargs={};
+    def kwargsproc(self,kwargs): return kwargs
+#    self __init__(self,*args,**kwargs): self.data=self.get_data()
+    def get_data(self): pass
+    def todf(self,data): return pd.DataFrame(data)
+    def save(self,*args,**kwargs):
+        df=self.todf(self.get_data())
+        kwargs=self.kwargsproc(kwargs)
+        df.to_csv(path_or_buf=os.path.join(os.getcwd()
+                                           ,self.__class__.__name__+'.tbl')
+                  ,index=False
+                  ,**kwargs)
+
+
+
+        
+ts=['ecg','sleep','power','spike','sin']
+
+class sampling(tbl):
+    
+    def get_data(self):
+        nm='names'
+        d=dict.fromkeys(data.get_kwargs(ts[0]).keys())#,[]) <- python gotcha!
+        for ak in d: d[ak]=[] #<-soln
+        d[nm]=[]
+        for s in ts:
+            kw=data.get_kwargs(s)
+            kw[nm]=s
+            for ak in kw: d[ak].append(kw[ak])
+        return pd.DataFrame.from_dict(d)
