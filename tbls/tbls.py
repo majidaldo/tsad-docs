ad4efa2d9cdb801b02b5dcf67412883b1b11609c
Majid d <majidaldosari-github@yahoo.com>
2016-01-18 18:10:27 -0500

    ts len

--- tbls/tbls.py
+++ tbls/tbls.py
@@ -31,12 +31,13 @@ ts=['ecg','sleep','power','spike','sin']
 class sampling(tbl):
     
     def get_data(self):
-        nm='name'
+        more=['name','length']
         d=dict.fromkeys(data.get_kwargs(ts[0]).keys())#,[]) <- python gotcha!
         for ak in d: d[ak]=[] #<-soln
-        d[nm]=[]
+        for am in more: d[am]=[]
         for s in ts:
             kw=data.get_kwargs(s)
-            kw[nm]=s
+            kw[more[0]]=s
+            kw[more[1]]=data.get_series(s).shape[0]
             for ak in kw: d[ak].append(kw[ak])
         return pd.DataFrame.from_dict(d)
